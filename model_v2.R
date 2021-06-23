4c851543b298cf524d0ac04dff4c63885ad72e93
LukasGuertler <65167044+LukasGuertler@users.noreply.github.com>
2021-06-23 10:53:06 +0200

    Problem with chance_event()

--- model_v2.R
+++ model_v2.R
@@ -13,59 +13,106 @@ model_function <- function(){
   investment_cost <- grass_planting_cost + protective_animal_cost + initial_fences_cost + 
     initial_truffle_cost + planting_tree_cost + initial_chicken_cost + chicken_mobile_cost
   
+  
+  # Maintenance
+  maintaining_trees <- vv(var_mean = maintaining_trees_cost,
+                          var_CV = 15,
+                          n = 20)
+  maintaining_trees_final <- Reduce("+", maintaining_trees)
+  
+  
+  maintaining_fences <- vv(var_mean = maintaining_fences_cost,
+                           var_CV = 15,
+                           n = 20)
+  maintaining_fences_final <- Reduce("+", maintaining_fences)
+  
+  
+  maintaining_chicken_mobile <- vv(var_mean = maintaining_chicken_mobile_cost,
+                                   var_CV = 15,
+                                   n = 20)
+  maintaining_chicken_mobile_final <- Reduce("+", maintaining_chicken_mobile)
+  
+  
+  replacing_chicken_final <- 19 * (133 * replacing_chicken_cost)
+  
+  
+  trees_to_replace_var <- vv(var_mean = trees_to_replace,
+                             var_CV = 100,
+                             n = 20)
+  replacing_trees <- vv(var_mean = replacing_trees_cost,
+                        var_CV = 20,
+                        n = 20)
+  replacing_trees_sum <- Map("*", trees_to_replace_var, replacing_trees)
+  replacing_trees_final <- Reduce("+", replacing_trees_sum)
+  
+  
+  feed <- vv(var_mean = feed_cost,
+             var_CV = 20,
+             n = 20)
+  feed_final <- Reduce("+", feed)
+  
+  
   # Income
-  nuts <- gompertz_yield(max_harvest = 300,
-                         time_to_first_yield_estimate = 4,
-                         first_yield_estimate_percent = 50,
-                         time_to_second_yield_estimate = 9,
-                         second_yield_estimate_percent = 100,
-                         n_years = 20)
-  for (value in nuts){
-  nut_income =+ value * nut_price 
-  }
 
   truffle <- gompertz_yield(max_harvest = 10,
                             time_to_first_yield_estimate = 1,
                             first_yield_estimate_percent = 50,
                             time_to_second_yield_estimate = 3,
                             second_yield_estimate_percent = 100,
-                            n_years = 20)
+                            n_years = 20,
+                            var_CV = 20)
   for (value in truffle){
     truffle_income =+ value * truffle_price
   }
+  
   
   eggs <- vv(var_mean = 8000,
              var_CV = 5, 
              n = 20)
   for (value in eggs){
-    egg_income =+ value * egg_price
+    egg_income =+ value * eggs_price
   }
   
-  wood_income <- 2 * wood_yield
-  
-  total_income <- nut_income + truffle_income + egg_income + wood_income + subsidies
-  
-  # Maintenance
-  
-  maintaining_trees <- vv(var_mean = maintaining_trees_cost,
-                          var_CV = 15,
-                          n = 20)
-  maintaining_trees_final <- Reduce("+", maintaining_trees)
-  
-  maintaining_fences <- vv(var_mean = maintaining_fences_cost,
-                          var_CV = 15,
-                          n = 20)
-  maintaining_fences_final <- Reduce("+", maintaining_fences)
-  
-  maintaining_chicken_mobile <- vv(var_mean = maintaining_chicken_mobile_cost,
-                           var_CV = 15,
-                           n = 20)
-  maintaining_chicken_mobile_final <- Reduce("+", maintaining_chicken_mobile)
+  #wood_income <- 2 * wood_yield
   
   
+  nuts <- gompertz_yield(max_harvest = 300,
+                         time_to_first_yield_estimate = 4,
+                         first_yield_estimate_percent = 50,
+                         time_to_second_yield_estimate = 10,
+                         second_yield_estimate_percent = 100,
+                         n_years = 20,
+                         var_CV = 20)
   
+  # Frost chance
+  # Hier hänge ich gerade...
+  # Der Ertrag von den Haselnüssen wurde ja schon mit gompertz() simuliert, jetzt will ich aber die Wahrscheinlichkeit, dass
+  # der Ertrag durch ein Spätfrostereignis ausfällt simulieren. Also einzelne Jahre mit 10%iger Wahrscheinlichkeit auf 0 setzen
+  # value_if und value_if_not müssen aber die gleiche Länge haben
   
+  nuts_frost <- chance_event(chance = 0.1,
+                             value_if = 0,
+                             value_if_not = nuts,
+                             n = 20)
   
+  for (value in nuts_frost){
+    nut_income =+ value * nut_price 
+    if (value != 0){
+      harvest_count =+ 1
+    }
+  }
+  
+  nut_harvest_cost_final <- harvest_count * nut_harvest_cost
+  
+    
+  # Final
+  
+  final_income <- nut_income + truffle_income + egg_income + subsidies
+  
+  final_maintenance <- maintaining_trees_final + maintaining_fences_final + maintaining_chicken_mobile_final +
+    replacing_cicken_final + replacing_trees_final + feed_final + nut_harvest_cost_final
+  
+  final_result <- final_income - final_maintenance - investment_cost
   
   return(list(final_result = final_result))
 }
