8a506e63cc1e5a0b62dd6aca869de4e28c099d4d
LukasGuertler <65167044+LukasGuertler@users.noreply.github.com>
2021-08-10 09:38:54 +0200

    evpi

--- visualization_analysis.R
+++ visualization_analysis.R
@@ -46,35 +46,22 @@ plot_distributions(
   xlab("Outcome distribution in €") +
   ylab("Probability density")
 
-# Decisions 2
-plot_distributions(
-  mcSimulation_object = simulation,
-  vars = c("d_1_inst_6","d_2_inst_6", "d_3_inst_6", "d_4_inst_6", "d_5_inst_6"),
-  method = "smooth_simple_overlay"
-) +
-  labs(title = "Scenarios compared to `only chickens`",
-       subtitle = "Accumulated values for 30 years - 10000 model runs") +
-  scale_fill_manual(
-    labels = c("Intervention 1...", "Intervention 2...",
-               "Intervention 3...", "Intervention 4...", "Intervention 5 instead of `only chickens`"),
-    values = c("red", "blue", "green", "orange", "purple", "grey"),
-    name = "Decision Options:"
-  ) +
-  theme(legend.position = "bottom")
 
 # Decisions 3
 plot_distributions(
   mcSimulation_object = simulation,
-  vars = c("d_4_inst_3", "d_5_inst_3", "d_5_inst_4"),
+  vars = c("d_5_inst_3"),
   method = "smooth_simple_overlay") +
-  labs(title = "Decisions",
+  labs(title = "Decisions Scenario 5 instead of 3",
        subtitle = "Accumulated values for 30 years - 10000 model runs") +
   scale_fill_manual(
-    labels = c("Scenario 5 - 3", "Scenario 5 - 3", "Scenario 5 - 4"),
+    labels = c("Scenario 5 instead of 3"),
     values = c("red", "blue", "green", "orange", "purple", "grey"),
     name = "Decision Options:"
   ) +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  xlab("Outcome distribution in €") +
+  ylab("Probability density")
 
 # Plot CO² Certificates ----
 plot_distributions(
@@ -189,55 +176,41 @@ pls_result3 <- plsr.mcSimulation(
 Pls_3 <- plot_pls(pls_result3, input_table = input_estimates, threshold = 0.8) + 
   ggtitle(label= "Truffle Trees + chicken (200 trees)")
 
+# outcome 4
 pls_result4<- plsr.mcSimulation(
-  object = simulation,
-  resultName = names(simulation$y)[3],
-  ncomp = 1
-)
-
-Pls_4<- plot_pls(pls_result3, input_table = input_estimates, threshold = 0.8) + 
-  ggtitle(label= "nuts+hay+chicken+truffle (70 trees)")
-
-pls_result5<- plsr.mcSimulation(
-  object = simulation,
-  resultName = names(simulation$y)[3],
-  ncomp = 1
-)
-
-Pls_5<- plot_pls(pls_result3, input_table = input_estimates, threshold = 0.8) + 
-  ggtitle(label= "nuts+chicken+truffle (200 trees)")
-
-
-
-# final plot
-Pls_combined <- ggarrange(Pls_2, Pls_1, Pls_3, Pls_4, Pls_5 + rremove("x.text"), 
-                          labels = c(" ", " ", " "," ", " "),
-                          ncol = 3, nrow = 1) +
-  ggtitle(label= "outcome_2")
-
-# outcome_4
-pls_result4 <- plsr.mcSimulation(
   object = simulation,
   resultName = names(simulation$y)[4],
   ncomp = 1
 )
 
-Pls_4 <- plot_pls(pls_result4, input_table = input_estimates, threshold = 0.8) + 
-  ggtitle(label= "4: Few Nuttrees + Truffle + Chicken")
+Pls_4<- plot_pls(pls_result4, input_table = input_estimates, threshold = 0.8) + 
+  ggtitle(label= "nuts+hay+chicken+truffle (70 trees)")
 
-# outcome_5
-pls_result5 <- plsr.mcSimulation(
+# outcome 5
+pls_result5<- plsr.mcSimulation(
   object = simulation,
   resultName = names(simulation$y)[5],
   ncomp = 1
 )
 
-Pls_5 <- plot_pls(pls_result5, input_table = input_estimates, threshold = 0.8) + 
-  ggtitle(label= "5: Many Nuttrees + Truffle + Chicken")
+Pls_5<- plot_pls(pls_result5, input_table = input_estimates, threshold = 0.8) + 
+  ggtitle(label= "nuts+chicken+truffle (200 trees)")
+
+# baseline
+pls_result6<- plsr.mcSimulation(
+  object = simulation,
+  resultName = names(simulation$y)[7],
+  ncomp = 1
+)
+
+Pls_6<- plot_pls(pls_result6, input_table = input_estimates, threshold = 0.8) + 
+  ggtitle(label= "nuts+chicken+truffle (200 trees)")
+
+
 
 # final plot
-Pls_combined <- ggarrange(Pls_1, Pls_2, Pls_3, Pls_4, Pls_5 + rremove("x.text"), 
-                          labels = c(" ", " ", " ", " ", " "),
+Pls_combined <- ggarrange(Pls_1, Pls_2, Pls_3, Pls_4, Pls_5, Pls_6 + rremove("x.text"), 
+                          labels = c(" ", " ", " ", " ", " ", " "),
                           ncol = 5, nrow = 1)
 annotate_figure(Pls_combined,
                 top = text_grob("Projection to Latent Structures analysis",
@@ -246,29 +219,23 @@ annotate_figure(Pls_combined,
 # EVPI ----
 # Use with caution!!! takes really long time to calculate!!!
 
-mcSimulation_table_1 <- data.frame(simulation$x, simulation$y[19])
-mcSimulation_table_2 <- data.frame(simulation$x, simulation$y[20])
-mcSimulation_table_3 <- data.frame(simulation$x, simulation$y[7])
-mcSimulation_table_4 <- data.frame(simulation$x, simulation$y[9])
-mcSimulation_table_5 <- data.frame(simulation$x, simulation$y[10])
-mcSimulation_table_6 <- data.frame(simulation$x, simulation$y[11])
-mcSimulation_table_7 <- data.frame(simulation$x, simulation$y[12])
-mcSimulation_table_8 <- data.frame(simulation$x, simulation$y[13])
+mcSimulation_table_1 <- data.frame(simulation$x, simulation$y[8])
+mcSimulation_table_2 <- data.frame(simulation$x, simulation$y[9])
+mcSimulation_table_3 <- data.frame(simulation$x, simulation$y[10])
+mcSimulation_table_4 <- data.frame(simulation$x, simulation$y[11])
+mcSimulation_table_5 <- data.frame(simulation$x, simulation$y[12])
+mcSimulation_table_6 <- data.frame(simulation$x, simulation$y[13])
 
-evpi_1 <- multi_EVPI(mc = mcSimulation_table_1, write_table = T, first_out_var = "d_5_inst_3")
-evpi_2 <- multi_EVPI(mc = mcSimulation_table_2, write_table = T, first_out_var = "d_4_inst_3")
-evpi_3 <- multi_EVPI(mc = mcSimulation_table_3, write_table = T, first_out_var = "d_5_inst_4")
-evpi_4 <- multi_EVPI(mc = mcSimulation_table_4, write_table = T, first_out_var = "d_5_inst_2")
-evpi_5 <- multi_EVPI(mc = mcSimulation_table_5, write_table = T, first_out_var = "d_3_inst_1")
-evpi_6 <- multi_EVPI(mc = mcSimulation_table_6, write_table = T, first_out_var = "d_3_inst_4")
-evpi_7 <- multi_EVPI(mc = mcSimulation_table_7, write_table = T, first_out_var = "d_3_inst_2")
-evpi_8 <- multi_EVPI(mc = mcSimulation_table_8, write_table = T, first_out_var = "d_3_inst_5")
+evpi_1 <- multi_EVPI(mc = mcSimulation_table_1, write_table = T, first_out_var = "decision_1")
+evpi_2 <- multi_EVPI(mc = mcSimulation_table_2, write_table = T, first_out_var = "decision_2")
+evpi_3 <- multi_EVPI(mc = mcSimulation_table_3, write_table = T, first_out_var = "decision_3")
+evpi_4 <- multi_EVPI(mc = mcSimulation_table_4, write_table = T, first_out_var = "decision_4")
+evpi_5 <- multi_EVPI(mc = mcSimulation_table_5, write_table = T, first_out_var = "decision_5")
+evpi_6 <- multi_EVPI(mc = mcSimulation_table_6, write_table = T, first_out_var = "d_5_inst_3")
 
-plot_evpi(evpi_1, decision_vars = "d_5_inst_3")
-plot_evpi(evpi_2, decision_vars = "d_4_inst_3")
-plot_evpi(evpi_3, decision_vars = "d_5_inst_4")
-plot_evpi(evpi_4, decision_vars = "d_5_inst_2")
-plot_evpi(evpi_5, decision_vars = "d_1_inst_3")
-plot_evpi(evpi_6, decision_vars = "d_4_inst_3")
-plot_evpi(evpi_7, decision_vars = "d_2_inst_3")
-plot_evpi(evpi_8, decision_vars = "d_5_inst_3")
+plot_evpi(evpi_1, decision_vars = "decision_1")
+plot_evpi(evpi_2, decision_vars = "decision_2")
+plot_evpi(evpi_3, decision_vars = "decision_3")
+plot_evpi(evpi_4, decision_vars = "decision_4")
+plot_evpi(evpi_5, decision_vars = "decision_5")
+plot_evpi(evpi_6, decision_vars = "d_5_inst_3")
